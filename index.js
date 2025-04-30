7fa4c44644c01b770fe8b52e22b1efd564698e9a
Dens97 <lessing.dennis@gmail.com>
2025-04-30 19:39:43 +0200

    Transfer data arrays to a different file

--- index.js
+++ index.js
@@ -1,60 +1,4 @@
-const celebrities = [
-  { url: "/images/celebrities/Angelina_Jolie.jpg", name: "Angelina Jolie" },
-  { url: "/images/celebrities/Blake_Lively.webp", name: "Blake Lively" },
-  { url: "/images/celebrities/Brad_Pitt.webp", name: "Brad Pitt" },
-  { url: "/images/celebrities/Chris_Hemsworth.webp", name: "Chris Hemsworth" },
-  { url: "/images/celebrities/Daniel_Radcliffe.jpg", name: "Daniel Radcliffe" },
-  { url: "/images/celebrities/Dua_Lipa.jpg", name: "Dua Lipa" },
-  { url: "/images/celebrities/Dwayne_Johnson.jpg", name: "Dwayne Johnson" },
-  { url: "/images/celebrities/Emma_Watson.webp", name: "Emma Watson" },
-  { url: "/images/celebrities/Florence_Pugh.jpg", name: "Florence Pugh" },
-  { url: "/images/celebrities/George_Clooney.jpg", name: "George Clooney" },
-  {
-    url: "/images/celebrities/Jennifer_Lawrence.jpg",
-    name: "Jennifer Lawrence",
-  },
-  {
-    url: "/images/celebrities/Leonardo_DiCaprio.jpg",
-    name: "Leonardo DiCaprio",
-  },
-  { url: "/images/celebrities/Natalie_Portman.jpg", name: "Natalie Portman" },
-  { url: "/images/celebrities/Oliva_Rodrigo.jpg", name: "Olivia Rodrigo" },
-  { url: "/images/celebrities/Ryan_Reynolds.webp", name: "Ryan Reynolds" },
-  {
-    url: "/images/celebrities/Scarlett_Johansson.webp",
-    name: "Scarlett Johansson",
-  },
-  {
-    url: "/images/celebrities/Timothée_Chalamet.jpg",
-    name: "Timothée Chalamet",
-  },
-  { url: "/images/celebrities/Tom_Hanks.jpg", name: "Tom Hanks" },
-  { url: "/images/celebrities/Will_Smith.jpg", name: "Will Smith" },
-  { url: "/images/celebrities/Zendaya.jpg", name: "Zendaya" },
-];
-
-const food = [
-  { url: "/images/food/Apfel.jpg", name: "Apfel" },
-  { url: "/images/food/Banane.jpg", name: "Banane" },
-  { url: "/images/food/Brokkoli.jpg", name: "Brokkoli" },
-  { url: "/images/food/Zitrone.jpg", name: "Zitrone" },
-  { url: "/images/food/Möhre.webp", name: "Möhre" },
-  { url: "/images/food/Tomate.jpg", name: "Tomate" },
-  { url: "/images/food/Zucchini.jpg", name: "Zucchini" },
-  { url: "/images/food/Ananas.jpg", name: "Ananas" },
-  { url: "/images/food/Kartoffel.webp", name: "Kartoffel" },
-  { url: "/images/food/Orange.webp", name: "Orange" },
-  { url: "/images/food/Knoblauch.png", name: "Knoblauch" },
-  { url: "/images/food/Bohne.jpg", name: "Bohne" },
-  { url: "/images/food/Avocado.jpg", name: "Avocado" },
-  { url: "/images/food/Mango.jpeg", name: "Mango" },
-  { url: "/images/food/Paprika.jpg", name: "Paprika" },
-  { url: "/images/food/Blumenkohl.jpg", name: "Blumenkohl" },
-  { url: "/images/food/Erdbeeren.webp", name: "Erdbeeren" },
-  { url: "/images/food/Pfirsich.jpeg", name: "Pfirsich" },
-  { url: "/images/food/Weintraube.jpg", name: "Weintraube" },
-  { url: "/images/food/Kürbis.webp", name: "Kürbis" },
-];
+import { celebrities, food } from "./data.js";
 
 const categories = [
   { name: "Schauspieler/Sänger", array: celebrities },
@@ -89,12 +33,6 @@ function drawCharacterList() {
         `;
   }
   characterList.innerHTML = characterString;
-  // const cards = document.querySelectorAll(".cardImage");
-  // for (let i = 0; i < cards.length; i++) {
-  //   cards[i].addEventListener("click", function (element) {
-  //     element.target.classList.toggle("eliminated");
-  //   });
-  // }
 }
 
 characterList.addEventListener("click", function (e) {
@@ -114,7 +52,10 @@ newGame();
 
 function drawCategorieDropdown() {
   let html = "";
-  for (category of categories) {
+  console.log("CategoriesArray: " + categories);
+  console.log(typeof categories);
+  for (let category of categories) {
+    console.log(category);
     html += `<option>${category.name}</option>`;
   }
   console.log(html);
